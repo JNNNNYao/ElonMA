5c7a7eb48843661aed016c59a40eac0c4a4e5752
姚潔恩 <matt1129yao@gmail.com>
2022-06-13 00:08:02 +0800

    demo: CD

--- bot/bot.py
+++ bot/bot.py
@@ -122,11 +122,8 @@ class Bot(discord.Client):
             print(message.channel.id)
             print(message.attachments[0].url)
             self.rabbitMQ_channel.basic_publish(exchange='', routing_key='bot2model', body=str(message.channel.id)+' +++ '+str(message.attachments[0].url))
-        elif cmd == 'doge':
-            url = key+currencies  
-            data = requests.get(url)
-            data = data.json()
-            await message.reply('current DOGE coin price: {}'.format(data['price']), mention_author=True)
+        elif cmd == 'CD':
+            await message.reply("Hi, everyone, I'm Elon Ma.", mention_author=True)
         else:
             await message.reply('unknown command!', mention_author=True)
 
